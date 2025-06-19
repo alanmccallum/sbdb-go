f7a79449e413b20058370689999de0acfa66b2f8
Alan McCallum <alanmccallum5@gmail.com>
2025-06-18 22:36:31 -0700

    Added documentation, removed DefaultClient, replaced with check in GetURL

--- client.go
+++ client.go
@@ -8,17 +8,14 @@ import (
 
 const Endpoint = "https://ssd-api.jpl.nasa.gov/sbdb_query.api"
 
+// Client wraps http.Client
+// The Client.Endpoint field can be set to use a custom api server.
 type Client struct {
 	http.Client
 	Endpoint string
 }
 
-func DefaultClient() *Client {
-	return &Client{
-		Endpoint: Endpoint,
-	}
-}
-
+// Get issues a GET to the Client.Endpoint using the specified Filter
 func (c *Client) Get(f Filter) (*http.Response, error) {
 	u, err := c.GetURL(f)
 	if err != nil {
@@ -28,12 +25,17 @@ func (c *Client) Get(f Filter) (*http.Response, error) {
 	return c.Client.Get(u.String())
 }
 
+// GetURL builds a *url.URL from the Client.Endpoint and the specified Filter
 func (c *Client) GetURL(f Filter) (*url.URL, error) {
 	v, err := f.Values()
 	if err != nil {
 		return nil, fmt.Errorf("error parsing filter: %w", err)
 	}
-	u, err := url.Parse(c.Endpoint)
+	ep := Endpoint
+	if c.Endpoint != "" {
+		ep = c.Endpoint
+	}
+	u, err := url.Parse(ep)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing endpoint: %w", err)
 	}
